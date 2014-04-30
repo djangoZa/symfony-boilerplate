ec9fdfe9ce31b285635f60e84c56524e8d79b510
Mike <mikejohnclarke@gmail.com>
2014-04-30 14:54:19 -0300

    Initializing boiler plate

---
+++ Vagrantfile
@@ -0,0 +1,10 @@
+VAGRANTFILE_API_VERSION = "2"
+Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+  config.vm.box = "precise64"
+  config.vm.provision "shell", :path => "Vagrantfile.sh", :args => ENV['APPLICATION_ENV']
+  config.vm.network "forwarded_port", host: 4567, guest: 80
+  config.vm.synced_folder "./", "/vagrant", id: "vagrant-root",
+    owner: "vagrant",
+    group: "www-data",
+    mount_options: ["dmode=775,fmode=664"]
+end
---
+++ Vagrantfile.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+#UPDATE APT REPO
+apt-get update
+apt-get dist-upgrade
+
+#INSTALL SERVICES
+apt-get install -y apache2 php5
+
+#LINK VAGRANT WWW TO APACHE WWW
+ln -fs /vagrant/project/web /var/www/symfony-boilerplate
+
+#ENABLE APACHE MOD REWRITE
+a2enmod rewrite
+
+#CREATE SYMFONY VIRTUAL HOST AND ENABLE
+cp /vagrant/VirtualHost /etc/apache2/sites-available/symfony-boilerplate
+a2ensite symfony-boilerplate
+
+#RESTART APACHE
+service apache2 restart
